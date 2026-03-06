c5e58db3cabef61f1fbf7b69f2b4899eb383a827
Fadillah-Dama <damarfky@gmail.com>
2026-03-07 02:08:15 +0700

    Jobsheet 4 - Praktikum 2.3 - Retrieving Aggregates

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <h1>Data User</h1>
-        <table border='1' cellpadding='2' cellspacing='0'>
+        {{-- <table border='1' cellpadding='2' cellspacing='0'>
             <tr>
                 <th>ID</th>
                 <th>Username</th>
@@ -20,6 +20,15 @@
                 <td>{{ $data->level_id }}</td>
             </tr>
             @endforeach
+        </table> --}}
+        <table border='1' cellpadding='1' cellspacing='0'>
+            <tr>
+                <th>Jumlah Pengguna</th>
+            </tr>
+            <tr>
+                <td>{{ $data }}</td>
+            </tr>
         </table>
+
     </body>
 </html>
